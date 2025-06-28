ddc5255973a5801d508e1851bb939fb7c84e4cf6
Madiyar Talgatuly <133092635+Madi4ar@users.noreply.github.com>
2025-06-28 16:27:48 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,51 @@ document.querySelector('.close-btn').onclick = function () {
   iframe.src = iframe.src;
   document.querySelector('.popup').classList.add('hidden');
 };
+
+document.querySelector('.join-waitlist').onclick = function () {
+  document.querySelector('.waitlist').classList.remove('hidden');
+};
+
+document.querySelector('.close-waitlist').onclick = function () {
+  document.querySelector('.waitlist').classList.add('hidden');
+};
+
+document
+  .getElementById('waitlistForm')
+  .addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const field = document.getElementById('field').value.trim();
+
+    const message = document.getElementById('message');
+    const error = document.getElementById('error');
+
+    message.classList.add('hidden');
+    error.classList.add('hidden');
+
+    if (!name || !email.includes('@') || !field) {
+      alert('Please fill out all fields correctly.');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://backend.framo.ai/api/waitlist/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, field }),
+      });
+
+      if (response.ok) {
+        message.classList.remove('hidden');
+        this.reset();
+      } else {
+        throw new Error('Server error');
+      }
+    } catch (err) {
+      error.classList.remove('hidden');
+    }
+  });
